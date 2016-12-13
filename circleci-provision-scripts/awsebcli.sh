8b64ddc1be289b0179289cb516f2293fe7e7f9a1
Nephtali Rodriguez <nrodriguez@xogrp.com>
2016-12-13 17:06:45 -0500

    Adds fix to properly install aws eb cli

--- circleci-provision-scripts/awsebcli.sh
+++ circleci-provision-scripts/awsebcli.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 function install_awsebcli() {
-    pip install --upgrade --user awsebcli
-    export PATH=~/.local/bin:$PATH
+    pip install --upgrade awsebcli
     echo 'export PATH=~/.local/bin:$PATH' >> ${CIRCLECI_HOME}/.circlerc
     echo 'export PATH=~/.local/bin:$PATH' >> ${CIRCLECI_HOME}/.bashrc
     source ${CIRCLECI_HOME}/.bashrc

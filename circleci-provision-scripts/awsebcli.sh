dfb8f7b2ab0474d23c51d29cb3239623bded537f
Nephtali Rodriguez <nrodriguez@xogrp.com>
2016-12-13 13:29:19 -0500

    Adds fix to properly load eb

--- circleci-provision-scripts/awsebcli.sh
+++ circleci-provision-scripts/awsebcli.sh
@@ -2,6 +2,9 @@
 
 function install_awsebcli() {
     pip install --upgrade --user awsebcli
+    export PATH=~/.local/bin:$PATH
+    echo 'export PATH=~/.local/bin:$PATH' >> ${CIRCLECI_HOME}/.circlerc
     echo 'export PATH=~/.local/bin:$PATH' >> ${CIRCLECI_HOME}/.bashrc
-    echo 'source ~/.bashrc &>/dev/null' >> ${CIRCLECI_HOME}/.bash_profile
+    source ${CIRCLECI_HOME}/.bashrc
+    source ${CIRCLECI_HOME}/.circlerc
 }
